b13f53368d01f18cbefc263067a074da3c4d2539
Wn13 <138977120+Anantya13@users.noreply.github.com>
2026-07-09 15:22:52 +0700

    Baguss

--- effect.js
+++ effect.js
@@ -5,9 +5,7 @@ $(window).load(function(){
 
 $('document').ready(function(){
     var vw;
-    $(window).resize(function(){
-         vw = $(window).width()/2;
-    });
+    $(window).resize(function(){ vw = $(window).width()/2; });
 
     $('#turn_on').click(function(){
         $('#bulb_yellow,#bulb_red,#bulb_blue,#bulb_green,#bulb_pink,#bulb_orange').addClass('bulb-glow-yellow bulb-glow-red bulb-glow-blue bulb-glow-green bulb-glow-pink bulb-glow-orange');
@@ -26,7 +24,6 @@ $('document').ready(function(){
         $(this).fadeOut('slow').delay(6000).promise().done(function(){ $('#balloons_flying').fadeIn('slow'); });
     });
 
-    // Fungsi loop untuk 9 balon
     function loop(id) {
         var randleft = 1000*Math.random();
         var randtop = 500*Math.random();
@@ -35,25 +32,33 @@ $('document').ready(function(){
 
     $('#balloons_flying').click(function(){
         $('.balloon-border').animate({top:-500},8000);
-        $('#b1,#b4,#b5,#b7,#b8').addClass('balloons-rotate-behaviour-one');
-        $('#b2,#b3,#b6,#b9').addClass('balloons-rotate-behaviour-two');
-        for(var i=1; i<=9; i++) { loop('#b'+i); }
+        for(var i=1; i<=9; i++) {
+            $('#b'+i).addClass('balloons-rotate-behaviour-one');
+            loop('#b'+i);
+        }
         $(this).fadeOut('slow').delay(5000).promise().done(function(){ $('#cake_fadein').fadeIn('slow'); });
+    });
+
+    // FUNGSI INI YANG TADI HILANG:
+    $('#cake_fadein').click(function(){
+        $('.cake').fadeIn('slow');
+        $(this).fadeOut('slow').delay(3000).promise().done(function(){ $('#light_candle').fadeIn('slow'); });
+    });
+
+    $('#light_candle').click(function(){
+        $('.fuego').fadeIn('slow');
+        $(this).fadeOut('slow').promise().done(function(){ $('#wish_message').fadeIn('slow'); });
     });
 
     $('#wish_message').click(function(){
         vw = $(window).width()/2;
         $('.balloons').stop();
-        // Atur posisi balon membentuk HBD dan FIRMAN
-        $('#b1').attr('id','b11').animate({top:150, left: vw-150},500);
-        $('#b2').attr('id','b22').animate({top:150, left: vw-50},500);
-        $('#b3').attr('id','b33').animate({top:150, left: vw+50},500);
-        $('#b4').attr('id','b44').animate({top:240, left: vw-250},500);
-        $('#b5').attr('id','b55').animate({top:240, left: vw-150},500);
-        $('#b6').attr('id','b66').animate({top:240, left: vw-50},500);
-        $('#b7').attr('id','b77').animate({top:240, left: vw+50},500);
-        $('#b8').attr('id','b88').animate({top:240, left: vw+150},500);
-        $('#b9').attr('id','b99').animate({top:240, left: vw+250},500);
+        var posHBD = [vw-150, vw-50, vw+50];
+        var posFIRMAN = [vw-250, vw-150, vw-50, vw+50, vw+150, vw+250];
+        
+        for(var i=1; i<=3; i++) $('#b'+i).attr('id','b'+i+i).animate({top:150, left: posHBD[i-1]},500);
+        for(var i=4; i<=9; i++) $('#b'+i).attr('id','b'+i+i).animate({top:240, left: posFIRMAN[i-4]},500);
+        
         $('.balloons').css('opacity','0.9');
         $('.balloons h2').fadeIn(3000);
         $(this).fadeOut('slow').delay(3000).promise().done(function(){ $('#story').fadeIn('slow'); });
@@ -63,12 +68,14 @@ $('document').ready(function(){
         $(this).fadeOut('slow');
         $('.cake').fadeOut('fast');
         $('.message').fadeIn('slow');
-        var i = 0;
+        var i = 1;
         function msgLoop (i) {
             $("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
                 i++;
-                $("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-                if(i < 10) { msgLoop(i); } // Sesuaikan angka 10 dengan jumlah paragraf Anda
+                if(i <= 10) { // Sesuaikan dengan jumlah <p> Anda
+                    $("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+                    msgLoop(i);
+                }
             });
         }
         msgLoop(1);
